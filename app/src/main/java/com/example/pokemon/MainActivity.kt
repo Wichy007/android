094dc4b7669fa5fd885ad1cb2c3a57f5bb549a68
wichy007 <naravitdansiriviroj@gmail.com>
2023-12-02 14:51:24 +0700

    pass value from layout to another layout

--- app/src/main/java/com/example/pokemon/MainActivity.kt
+++ app/src/main/java/com/example/pokemon/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.pokemon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.pokemon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    // สร้างตัวแปร binding จริงๆตัวแปรชื่ออื่นได้แต่ธรรมเนียมใช้ชื่อนี้ ขึ้นมา type ชื่อเดียวกับ layout แต่ตัวใหญ่ (Android studio จะแนะนำให้)
     private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +19,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.PokeBall.setOnClickListener{
-            Log.e(MainActivity::class.java.simpleName, "on click")
+//            Log.e(MainActivity::class.java.simpleName, "on click")
+            val intent = Intent(this, PokemonDetailActivity::class.java)
+            intent.putExtra("Name", "Naravit")
+            startActivity(intent)
+        }
+
+        binding.BackPack.setOnClickListener{
+            val intent = Intent(this, PokemonCollectionActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/pokemon/PokemonDetailActivity.kt
+++ app/src/main/java/com/example/pokemon/PokemonDetailActivity.kt
@@ -2,10 +2,18 @@ package com.example.pokemon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pokemon.databinding.ActivityPokemonDetailBinding
 
 class PokemonDetailActivity : AppCompatActivity() {
+    lateinit var binding: ActivityPokemonDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pokemon_detail)
+        binding = ActivityPokemonDetailBinding.inflate(layoutInflater)
+//        setContentView(R.layout.activity_pokemon_detail)
+        setContentView(binding.root)
+
+        val name  = intent.getStringExtra("Name")
+
+        binding.nameTv.text = name
     }
 }
